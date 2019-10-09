c884b62eb52866325a9833ee9262b13705e01ac7
Matt Freeman <mathew.s.l.freeman@gmail.com>
2019-10-09 10:38:53 +0000

    step9

--- directory.rb
+++ directory.rb
@@ -1,21 +1,35 @@
-# frozen_string_literal: true
+def interactive_menu
+  students = []
+  loop do
+    puts "1. Input the students."
+    puts "2. Show the students."
+    puts "9. Exit."
+    selection = gets.chomp
+    case selection
+      when "1"
+        students = input_students
+      when "2"
+        print_header
+        print(students)
+        print_footer(students)
+      when "9"
+        exit
+      else
+        puts "I don't know what you mean, please try again."
+    end
+  end
+end
 
 def input_students
   puts "Please enter the names of the students."
   puts "To finish, just hit return twice."
-  # create an empty array
   students = []
-  # get the first name
   name = gets.chomp
-  # while the name is not empty, repeat this code
   until name.empty?
-  # add the student hash to the array
     students << { name: name, cohort: :november }
     puts "Now we have #{students.count} students."
-  # get another name from the user
     name = gets.chomp
   end
-  # return the array of students
   students
 end
 
@@ -25,8 +39,12 @@ def print_header
 end
 
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]}, (#{student[:cohort]} cohort)"
+  if students.count == 0
+    puts "There are no students enrolled."
+  elsif
+    students.each do |student|
+      puts "#{student[:name]}, (#{student[:cohort]} cohort)"
+    end
   end
 end
 
@@ -34,7 +52,4 @@ def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+interactive_menu
